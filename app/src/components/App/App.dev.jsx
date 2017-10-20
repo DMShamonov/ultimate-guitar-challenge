12f55cbed77671c545f2d4c372fa3dd6e007058d
dmshamonov <dmshamonov@yandex.ru>
2017-10-21 01:23:31 +0800

    Update App.dev

--- app/src/components/App/App.dev.jsx
+++ app/src/components/App/App.dev.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import AppRouter from 'components/AppRouter';
 import LocalStorageController from 'controllers/LocalStorage';
 import configureStore from 'store';
+import DevTools from 'components/DevTools';
 
 const releases = new LocalStorageController().getItem('releases') || undefined;
 const store = configureStore({ releases: releases && JSON.parse(releases) });
@@ -14,6 +15,7 @@ const AppComponent = () => (
     <Provider store={store}>
       <div style={{ width: '100%', height: '100%' }}>
         <AppRouter />
+        <DevTools />
       </div>
     </Provider>
   </BrowserRouter>
